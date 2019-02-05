bf0fb7a7f8fd528ef64f642fc73cc2c323928c93
Reetta <reetta.karinluoma@gmail.com>
2019-02-05 12:49:38 +0200

    Arvostelun muokkaaminen ja poistaminen onnistuu

--- application/ratings/views.py
+++ application/ratings/views.py
@@ -13,16 +13,14 @@ def ratings_index():
 def ratings_form():
     return render_template("ratings/new.html", form = RatingForm())
 
-@app.route("/ratings/<rating_id>/", methods=["GET"])
+@app.route("/ratings/<rating_id>", methods=["GET"])
 @login_required
-def ratings_modify(rating_id):
+def ratings_editform(rating_id):
     r = Rating.query.get(rating_id)
     form = RatingEditForm(obj=r)
     #form = RatingEditForm(r.beer, r.rating, r.comment, r.flavor)
 
-    return render_template("ratings/new.html", form = form)
-  
-    #return redirect(url_for("tasks_index"))
+    return render_template("ratings/edit.html", form = form, id = rating_id)
 
 @app.route("/ratings/", methods=["POST"])
 @login_required
@@ -38,6 +36,37 @@ def ratings_create():
     r.account_id = current_user.id
     
     db.session().add(r)
+    db.session().commit()
+  
+    return redirect(url_for("ratings_index"))
+
+@app.route("/ratings/<rating_id>/", methods=["POST"])
+@login_required
+def ratings_edit(rating_id):
+    form = RatingForm(request.form)
+
+    if not form.validate():
+        return render_template("ratings/edit.html", form = form, id = rating_id)
+
+    r = Rating.query.get(rating_id)
+    r.beer = form.beer.data
+    r.rating = form.rating.data
+    r.comment = form.comment.data
+    r.flavor = form.flavor.data
+    #r.rating = form.rating.data
+    #r = Rating(request.form.get("beer"), request.form.get("rating"))
+    r.account_id = current_user.id
+    
+    db.session().commit()
+  
+    return redirect(url_for("ratings_index"))
+
+@app.route("/ratings/delete/<rating_id>/", methods=["POST"])
+@login_required
+def ratings_delete(rating_id):
+
+    r = Rating.query.get(rating_id)
+    db.session().delete(r)
     db.session().commit()
   
     return redirect(url_for("ratings_index"))
